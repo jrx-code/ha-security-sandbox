247cfcaf5a92b40e2716caf4de1e040b5a3a89ea
JI Engineering <76726185+jrx-code@users.noreply.github.com>
2026-09-18 12:44:51 +0800

    chore: bump to 0.21.1, addon alerts_enabled option, CHANGELOG for #3

--- ha-sandbox/run.sh
+++ ha-sandbox/run.sh
@@ -47,6 +47,7 @@ if [ "$STANDALONE" = "false" ]; then
     fi
     export SANDBOX_MQTT_ENABLED="$(bashio::config 'mqtt_enabled')"
     export SANDBOX_MQTT_TLS="$(bashio::config 'mqtt_tls')"
+    export SANDBOX_ALERTS_ENABLED="$(bashio::config 'alerts_enabled')"
 
     # HA API via Supervisor
     export SANDBOX_HA_URL="http://supervisor/core"
